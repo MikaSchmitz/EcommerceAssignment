a538fe1d71f78d69dbddad692a9cfe8b165bd7b1
Mika Schmitz <47559434+MikaSchmitz@users.noreply.github.com>
2024-09-09 19:47:57 +0200

    Added base entity

---
+++ OrderProcessing/Data/Models/BaseDatabaseContent.cs
@@ -0,0 +1,26 @@
+﻿using OrderProcessing.Data.Interfaces;
+
+namespace OrderProcessing.Data.Models
+{
+    public abstract class BaseDatabaseContent : IDatabaseContent
+    {
+        /// <summary>
+        /// The primary key of the entity
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The UTC date and time on which the entity was created
+        /// </summary>
+        public DateTime CreatedOn { get; set; }
+
+        /// <summary>
+        /// Base constructor sets the default and initial values of the base entity
+        /// </summary>
+        protected BaseDatabaseContent()
+        {
+            Id = Guid.NewGuid();
+            CreatedOn = DateTime.UtcNow;
+        }
+    }
+}
